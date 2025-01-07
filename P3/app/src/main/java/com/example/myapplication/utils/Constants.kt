0eb8e0b91bd16e2f34ed9a58770012293273f3f1
SergioHrvas <sergiohervas18@gmail.com>
2025-01-07 14:46:52 +0100

    Nuevas ips

--- P3/app/src/main/java/com/example/myapplication/utils/Constants.kt
+++ P3/app/src/main/java/com/example/myapplication/utils/Constants.kt
@@ -2,7 +2,8 @@ package com.example.myapplication.utils
 
 object Constants {
     //const val SERVER_URL = "http://10.0.2.2:8080/"
-    //const val SERVER_URL = "http://192.168.1.136:8080/" //PORTATIL SERGIO
-    const val SERVER_URL = "http://192.168.1.143:8080/" //SOBREMESA SERGIO
+    //const val SERVER_URL = "http://192.168.1.136:8080/" //PORTATIL SERGIO WIFI
+    const val SERVER_URL = "http://192.168.233.232:8080/" //PORTATIL SERGIO RED
+    //const val SERVER_URL = "http://192.168.1.143:8080/" //SOBREMESA SERGIO
     //const val SERVER_URL = "http://192.168.81.232:8080/" //PORTATIL SERGIO UGR
 }
